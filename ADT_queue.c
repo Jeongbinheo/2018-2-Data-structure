62dd185a001cedd32e8e6de8b2ddc5836f3faaca
JEONG BIN HEO <45026816+Jeongbinheo@users.noreply.github.com>
2020-11-09 13:52:12 +0900

    Update ADT_queue.c

--- ADT_queue.c
+++ ADT_queue.c
@@ -1,63 +1,41 @@
 #include "ADT_queue.h"
 
-QUEUE* create_queue(){
+QUEUE* create_queue() {
 	QUEUE* queue;
-	queue=(QUEUE*)malloc(sizeof(QUEUE));
-	if(queue){	
-	
+	queue = (QUEUE*)malloc(sizeof(QUEUE));
+
+	if(queue) {
 		queue->front = NULL;
 		queue->rear = NULL;
 		queue->count = 0;
 		return queue;
-
-	}else{
+	} else {
 		return NULL;
 	}
-
 }
 
 bool
-enqueue(
-	QUEUE* queue,//target queue
-	 void* in //Data
-){
-	QUEUE_NODE* new_node=(QUEUE_NODE*)malloc(sizeof(QUEUE_NODE));
-	if(new_node == NULL){
+enqueue (
+	QUEUE* queue, //target queue 
+	void* in      //data	
+) {
+	QUEUE_NODE* new_node;
+	new_node =(QUEUE_NODE*) malloc(sizeof(QUEUE_NODE));
+	if(!new_node)
 		return false;
-	}else{ 
-		
-	new_node->data_ptr = in;
-	new_node->next = NULL;
 
+	new_node->data_ptr = in;
+	new_node->next= NULL;
+	
 	if(queue->count == 0){
-		queue->front=new_node;	
-	}
-	else{
+		queue->front = new_node;
+	} else{
 		queue->rear->next = new_node;
 	}
-		
-	queue->rear=new_node;
+	
+	queue->rear = new_node;
 	queue->count++;
 	return true;
-	}
 }
 
-void* dequeue(QUEUE* queue){
-	
-	QUEUE_NODE* temper;
-		 
-	
-
-
-
-
-
-
-
-
-
-
-
-
-}
 
